ca3f33fadf72d0adfc233ac9659e2c1333b0a809
Rumen Neshev <rstar2@abv.bg>
2018-07-02 23:39:37 +0300

    check GitLab SSh push

--- routes/view/common.js
+++ routes/view/common.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
 
+    // TODO: move to aws only - currently just for testing
     app.get('/download/:videoId', (req, res) => {
         const videoId = req.params.videoId;
         // Render the download page template.
