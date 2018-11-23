e5b99686038f2ea6f94671c506a6b02ed6d56403
Rumen Neshev <rstar2@abv.bg>
2018-11-23 23:49:32 +0200

    use docker image

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+exodus --tarball ffmpeg | tar -zx
+
+rm -f aws-artifact.zip
+zip --symlinks --recurse-paths aws-artifact.zip src/ node_modules/ exodus/
+
+exec "$@"
--- docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-exodus --tarball ffmpeg | tar -zx
-
-rm -f aws-artifact.zip
-zip --symlinks --recurse-paths aws-artifact.zip src/ node_modules/ exodus/
